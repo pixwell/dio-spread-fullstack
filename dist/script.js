4f769549c5ac9112ef24450f418eac92c388f81a
Marcia Silva <contato.pix@gmail.com>
2026-08-14 19:38:44 -0300

    Transpilação após o ajuste do tsconfig.json

--- dist/script.js
+++ dist/script.js
@@ -1,4 +1,11 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-(function () { })();
+(function () {
+    //const $ = (query: string) => document.querySelector(query) 
+    const btn = document.getElementById('cadastrar');
+    const nome = document.getElementById('nome');
+    const placa = document.getElementById('placa');
+    btn?.addEventListener('click', () => {
+    });
+    console.log(btn, nome, placa);
+})();
+export {};
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -1,1 +1,12 @@
-(function(){})();
+(function(){
+    //const $ = (query: string) => document.querySelector(query) 
+
+    const btn = document.getElementById('cadastrar')
+    const nome = document.getElementById('nome')
+    const placa = document.getElementById('placa')
+
+    btn?.addEventListener('click', () => {
+    })
+
+    console.log(btn, nome, placa)
+})();
