d4a75247081670ff9b3849f0d21c5e677ed4b7e3
Marcia Silva <contato.pix@gmail.com>
2026-08-14 21:49:45 -0300

    Programação da base da aplicação: Variáveis, funçoes iniciais e event listener no botão de envio

--- script.ts
+++ script.ts
@@ -1,12 +1,34 @@
-(function(){
-    //const $ = (query: string) => document.querySelector(query) 
+interface VeiculoProps{
+    nome: string;
+    placa: string;
+    entrada: Date;
+}
 
+(function(){
+    //const $ = (query: string): HTMLInputElement | null => document.querySelector(query) 
     const btn = document.getElementById('cadastrar')
-    const nome = document.getElementById('nome')
-    const placa = document.getElementById('placa')
+    const nome = document.getElementById('nome') as HTMLInputElement
+    const placa = document.getElementById('placa') as HTMLInputElement
+
+    function patio(){
+        function ler(){}
+        function adicionar(veiculo: VeiculoProps){}
+        function remover(){}
+        function render(){}
+
+        return {ler, adicionar, remover, render}    
+    }
 
     btn?.addEventListener('click', () => {
+        const nomeField = nome?.value
+        const placaField = placa?.value.toUpperCase()
+
+        if(!nomeField || !placaField){
+            alert('Os campos nome e placa são obrigatórios!')
+            return
+        }
+
+        patio().adicionar({nome: nomeField, placa: placaField, entrada: new Date()})
     })
 
-    console.log(btn, nome, placa)
 })();
