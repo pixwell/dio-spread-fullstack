bb0ec0c9739307a19ec0dc625924aaf220dcc906
Marcia Silva <contato.pix@gmail.com>
2026-08-14 21:51:31 -0300

    Arquivos Dist

--- dist/script.js
+++ dist/script.js
@@ -1,11 +1,24 @@
 (function () {
-    //const $ = (query: string) => document.querySelector(query) 
+    //const $ = (query: string): HTMLInputElement | null => document.querySelector(query) 
     const btn = document.getElementById('cadastrar');
     const nome = document.getElementById('nome');
     const placa = document.getElementById('placa');
+    function patio() {
+        function ler() { }
+        function adicionar(veiculo) { }
+        function remover() { }
+        function render() { }
+        return { ler, adicionar, remover, render };
+    }
     btn?.addEventListener('click', () => {
+        const nomeField = nome?.value;
+        const placaField = placa?.value.toUpperCase();
+        if (!nomeField || !placaField) {
+            alert('Os campos nome e placa são obrigatórios!');
+            return;
+        }
+        patio().adicionar({ nome: nomeField, placa: placaField, entrada: new Date() });
     });
-    console.log(btn, nome, placa);
 })();
 export {};
 //# sourceMappingURL=script.js.map
